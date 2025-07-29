5476fedf0832295d50f44c944c0c623cce30281a
saravanan2821 <saravanan282155@gmail.com>
2025-07-29 11:29:43 +0530

    Added new commit

--- Datatypes.py
+++ Datatypes.py
@@ -3,7 +3,7 @@ age =int(input("Enter your Age:"))
 height =float(input("Enter your Height:"))
 is_student =input("Are you Student (Yes/No):").lower() == "yes"
 
-print("------Your Details------")
+print("------Your Details------")  #Datatypes
 print("Name:",name)
 print("Age:",age)
 print("Height:",height)
---
+++ Sample_Pgm.py
@@ -0,0 +1,4 @@
+name = input("Enter Your Name:")
+age = int(input("Enter your Age:"))
+
+print(f" Your Name is {name}! Your age is {age} years old!")
