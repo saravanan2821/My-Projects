bae8820640fafb21b17149850639838b942477c0
saravanan2821 <saravanan282155@gmail.com>
2025-07-30 10:32:06 +0530

    New added commit

--- ConditionalStatement.py
+++ ConditionalStatement.py
@@ -1,8 +1,28 @@
 age = int(input("Enter your Age:"))
 
-if age < 18:
-    print("You are a minor.")
-elif age >= 18 and age < 60:
-    print("You are an adult.")
+if age >= 18:
+    print("Eligible for voting.")
 else:
-    print("You are a Senior citizen.")
+    print("Not Eligible for voting.")
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Even Number")
+
+else:
+    print("Odd Number")
+
+mark = int(input("Enter Your Mark : "))
+
+if mark >=90:
+    print("Grade A")
+elif mark >=75:
+    print("Grade B")
+elif mark >=60:
+    print("Grade C")
+elif mark >=40:
+    print("Pass")
+else:
+    print("Fail")
+
---
+++ Simultaneous_Assignments.py
@@ -0,0 +1,11 @@
+x,y,z = 5,10,15
+
+x,y,z= z,x,y
+
+print(x,y,z)
+
+a, b, c = 1, 2, 3
+
+a, b, c = b, c, a
+
+print(a, b, c)
