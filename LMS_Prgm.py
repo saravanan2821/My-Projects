514c087c4796bda9a79f505fa7c1da7a445018af
saravanan2821 <saravanan282155@gmail.com>
2025-08-04 11:29:57 +0530

    Lms_Task

---
+++ LMS_Prgm.py
@@ -0,0 +1,8 @@
+# LMS 5th Task
+
+name =input("Enter your name : ")
+age = int(input("Enter your age : "))
+favorite_number = int(input("Enter your favorite number : "))
+
+print(f"Hello, {name}! You are {age} years old, and your favorite number is {favorite_number}.")
+
