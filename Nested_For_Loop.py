45290191f65c31c7aaa1a4a655b8ade5acd72e93
saravanan2821 <saravanan282155@gmail.com>
2025-08-06 18:22:40 +0530

    Nested

---
+++ Nested_For_Loop.py
@@ -0,0 +1,15 @@
+"""for b in range(1,11):
+    for d in range(1,21):
+        print(d, end=" ")
+    print(" ")"""
+
+"""
+for k in range(-6,-1):
+    for s in range(1,6):
+        print(s,"*", end="")
+
+    print()"""
+
+
+for k in range(1,6):
+    print
