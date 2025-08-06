cbc994a63f10b3c9a681603330c4d86728f34c5a
saravanan2821 <saravanan282155@gmail.com>
2025-08-06 18:19:52 +0530

    Forloop

---
+++ For_Loop_Prgm.py
@@ -0,0 +1,39 @@
+"""for name in ["Saravanan",24,"Python-Student"]:
+    print(name)"""
+from binascii import a2b_base64
+
+"""
+for name in"Saravanan""Python":
+    print(name)"""
+"""
+for name in {"Saravanan",24,"Python-Student"}:
+    print(name)"""
+"""
+for name in {"name" :"Saravanan", "age" :24 , "Course" : "Python-Student"}:
+    print(name)
+"""
+
+"""num1 =int(input("Enter the starting number "))
+num2 =int(input("Enter the ending number"))
+
+
+for i in range(num1 * 7,num2 * 7):
+    print(f"{i} x 7 = {num1,num2}")"""
+
+"""for i in range (1 , 99):
+    print("* "*i)"""
+"""
+z = {2}
+
+print(type(z))
+"""
+"""
+num = int(input("Enter the number : "))
+
+for a in range(1,num+1):
+    fact=fact*a
+
+print(fact)
+"""
+
+box = ["Elephant","Egg","Apple","Ant"]
