ffd10d5d58fe398ae863ba78ea4dd51906f3c947
saravanan2821 <saravanan282155@gmail.com>
2025-08-06 18:24:06 +0530

    Whileloop

---
+++ While_Loop_Prgm.py
@@ -0,0 +1,21 @@
+"""
+
+"""
+
+"""
+for num in range(1,100):
+    if num==25:
+        break
+    print(num,end=" ")"""
+"""
+for num in range(1,100):
+    if num==25:
+        continue
+    print(num,end=" ")"""
+num=0
+while num<=20:
+    num+=1
+    if num==15:
+        continue
+
+    print(num,end=" ")
